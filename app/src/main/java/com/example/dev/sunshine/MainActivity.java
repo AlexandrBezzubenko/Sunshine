36bfed09557041b20263e292df8bcde1d3af0622
Alexander Bezzubenko <abezzubenko@zeuselectronics.eu>
2016-11-02 17:24:26 +0200

    lesson 3/20

--- app/src/main/java/com/example/dev/sunshine/MainActivity.java
+++ app/src/main/java/com/example/dev/sunshine/MainActivity.java
@@ -1,18 +1,34 @@
 package com.example.dev.sunshine;
 
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new ForecastFragment()).commit();
         }
     }
 
+
+    public void onCreateOptionMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.forecast_fragment, menu);
+    }
+
+
+    public boolean onOptionItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
